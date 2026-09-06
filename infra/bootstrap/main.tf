dfaf9399dfefb48ecacb590989218359a45f4c83
nikmar0808 <marathester@gmail.com>
2026-09-06 10:25:54 +0530

    ci: TruffleHog and Trivy scanning, OIDC ECR push, SSM deploy reading secrets from Parameter Store

--- infra/bootstrap/main.tf
+++ infra/bootstrap/main.tf
@@ -14,7 +14,7 @@ variable "github_repo" { default = "nikmar0808/enterprise-integration-project" }
 resource "aws_iam_openid_connect_provider" "github" {
   url             = "https://token.actions.githubusercontent.com"
   client_id_list  = ["sts.amazonaws.com"]
-  thumbprint_list = ["6938fd4d98bab03faadb97b34396831e3780aea1"]
+  thumbprint_list = []
 }
 
 data "aws_iam_policy_document" "gha_trust" {
